de7724bd985ef1a4e766a6a0cad17953ac8dc3e2
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-04-11 10:52:16 -0300

    Update atv4.py

--- L2 - PY/atv4.py
+++ L2 - PY/atv4.py
@@ -1,8 +1,8 @@
-# Lê os números inteiros A e B 
+#lê os números inteiros A e B 
 a = int(input("Digite o valor de A: "))
 b = int(input("Digite o valor de B: "))
 
-# Verifica se A é divisível por B
+#verifica se A é divisível por B
 if a % b == 0:
     print("A é divisível por B")
 else:
