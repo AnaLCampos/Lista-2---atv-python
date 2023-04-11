faa8b6b3cdd3ab18fa9e603239f1cf294e03d454
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-04-11 10:51:45 -0300

    Update atv2.py

--- L2 - PY/atv2.py
+++ L2 - PY/atv2.py
@@ -1,7 +1,7 @@
-num1 = int(input("Digite o primeiro número inteiro: "))
-num2 = int(input("Digite o segundo número inteiro: "))
+Num1 = int(input("Digite o primeiro número inteiro: "))
+Num2 = int(input("Digite o segundo número inteiro: "))
 
-if num1 == num2:
+if Num1 == Num2:
     print("Os números são iguais.")
 else:
     print("Os números são diferentes.")
